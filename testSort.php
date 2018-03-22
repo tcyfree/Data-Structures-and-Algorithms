365815b6722d63852c6373571d1314df7ca8176e
tcyfree <1946644259@qq.com>
2018-03-22 11:59:27 +0800

    README.md 对文件和其函数做详细说明

--- testSort.php
+++ testSort.php
@@ -7,40 +7,42 @@
  */
 require_once "Helper.php";
 
-$n = 100;
+$n = 2000;
 $rangeL = 0;
-$rangeR = 9;
+$rangeR = 100;
 $swapTimes = 10;
-$sortName1 = 'insertionSort_test';
-$sortName2 = 'insertionSort';
+$sortName1 = 'shellSort';
+//$sortName2 = 'insertionSort';
 
-echo '测试1 一般测试,完全随机'.'<br />';
+echo '测试1 一般测试,完全随机'.'<p>';
 $arr = generateRandomArray($n,$rangeL,$rangeR);
+print_r($arr);
+echo '<p>';
 print_r('判断数组是否有序：'.isSorted(testSort($sortName1, $arr)));
 echo '<br />';
-$sortName = 'insertionSort';
-print_r('判断数组是否有序：'.isSorted(testSort($sortName2, $arr)));
+//$sortName = 'insertionSort';
+//print_r('判断数组是否有序：'.isSorted(testSort($sortName2, $arr)));
 
-echo '<br />';
-echo '<br />'.'测试2 有序性更强的测试'.'<br />';
-
-$rangeL = 0;
-$rangeR = 3;
-$arr = generateRandomArray($n,$rangeL,$rangeR);
-$arr2 = $arr;
-print_r('判断一个数组是否有序：'.isSorted(testSort($sortName1, $arr)));
-
-echo '<br />';
-
-print_r('判断一个数组是否有序：'.isSorted(testSort($sortName2, $arr2)));
-echo '<br />';
-echo '<br />'.'测试3 测试近乎有序的数组'.'<br />';
-$swapTimes = 10;
-$arr = generateNearlyOrderedArray($n,$swapTimes);
-$arr2 = $arr;
-print_r('判断一个数组是否有序：'.isSorted(testSort($sortName1, $arr)));
-
-echo '<br />';
-
-$sortName = 'insertionSort';
-print_r('判断一个数组是否有序：'.isSorted(testSort($sortName2, $arr2)));
+//echo '<br />';
+//echo '<br />'.'测试2 有序性更强的测试'.'<br />';
+//
+//$rangeL = 0;
+//$rangeR = 3;
+//$arr = generateRandomArray($n,$rangeL,$rangeR);
+//$arr2 = $arr;
+//print_r('判断一个数组是否有序：'.isSorted(testSort($sortName1, $arr)));
+//
+//echo '<br />';
+//
+//print_r('判断一个数组是否有序：'.isSorted(testSort($sortName2, $arr2)));
+//echo '<br />';
+//echo '<br />'.'测试3 测试近乎有序的数组'.'<br />';
+//$swapTimes = 10;
+//$arr = generateNearlyOrderedArray($n,$swapTimes);
+//$arr2 = $arr;
+//print_r('判断一个数组是否有序：'.isSorted(testSort($sortName1, $arr)));
+//
+//echo '<br />';
+//
+//$sortName = 'insertionSort';
+//print_r('判断一个数组是否有序：'.isSorted(testSort($sortName2, $arr2)));
